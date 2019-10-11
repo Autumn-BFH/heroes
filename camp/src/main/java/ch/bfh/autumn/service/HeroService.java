fc9dacbde21e5bfc69f737eef95df56140c788f2
Berger Adrian <adrian.berger.3@students.bfh.ch>
2019-10-11 11:37:42 +0200

    Add model & service Fix .gitignore Add compiler source & target for maven compiler

---
+++ camp/src/main/java/ch/bfh/autumn/service/HeroService.java
@@ -0,0 +1,8 @@
+package ch.bfh.autumn.service;
+
+import ch.bfh.autumn.model.Hero;
+
+public interface HeroService {
+
+    Hero createHero(String name);
+}
---
+++ camp/src/main/java/ch/bfh/autumn/service/PartyService.java
@@ -0,0 +1,8 @@
+package ch.bfh.autumn.service;
+
+import ch.bfh.autumn.model.Party;
+
+public interface PartyService {
+
+    Party createParty(String name);
+}
