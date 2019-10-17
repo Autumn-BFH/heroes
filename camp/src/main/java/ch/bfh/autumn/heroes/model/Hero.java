91e10435c81c4b01f5b606e8bcc5dbde3f966987
Fabian Küng <fabian.kueng@inpeek.ch>
2019-10-17 13:49:57 +0200

    add models and interfaces from exercise

---
+++ camp/src/main/java/ch/bfh/autumn/heroes/model/Hero.java
@@ -0,0 +1,12 @@
+package ch.bfh.autumn.heroes.model;
+
+import lombok.Data;
+
+@Data
+public class Hero {
+
+  private String name;
+  private int atk;
+  private int def;
+  private int hp;
+}
---
+++ camp/src/main/java/ch/bfh/autumn/heroes/service/PartyService.java
@@ -0,0 +1,8 @@
+package ch.bfh.autumn.heroes.service;
+
+import ch.bfh.autumn.heroes.model.Party;
+
+public interface PartyService {
+
+  Party createParty(String name);
+}
