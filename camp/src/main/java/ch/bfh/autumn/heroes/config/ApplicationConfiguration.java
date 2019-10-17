dfb06149c7237c06bc4a335d912bfe6827cd8200
Fabian Küng <fabian.kueng@inpeek.ch>
2019-10-17 13:50:58 +0200

    add application configuration

---
+++ camp/src/main/java/ch/bfh/autumn/heroes/config/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package ch.bfh.autumn.heroes.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ch.bfh.autumn.heroes")
+public class ApplicationConfiguration {
+
+}
