19df7b425db73d6a8554be8d4db84f25ce2a5df3
Fabian Küng <fabian.kueng@inpeek.ch>
2019-10-18 12:29:58 +0200

    refactor Application class to use SpringBootApplication annotation instead of manual Spring Context

--- camp/src/main/java/ch/bfh/autumn/heroes/Application.java
+++ camp/src/main/java/ch/bfh/autumn/heroes/Application.java
@@ -1,19 +1,24 @@
 package ch.bfh.autumn.heroes;
 
-import ch.bfh.autumn.heroes.config.ApplicationConfiguration;
 import ch.bfh.autumn.heroes.service.PartyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
   public static void main(String[] args) {
-    AbstractApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-        ApplicationConfiguration.class);
+    SpringApplication.run(Application.class, args);
+  }
 
-    PartyService partyService = applicationContext.getBean(PartyService.class);
-    partyService.createParty("Party 1");
-
-    applicationContext.close();
+  @Bean
+  public CommandLineRunner demo(PartyService partyService) {
+    return (args) -> {
+      partyService.createParty("Party 1");
+    };
   }
 }
