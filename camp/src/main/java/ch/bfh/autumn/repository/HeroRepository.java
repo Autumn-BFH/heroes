fbe3156dc6fbb1b5b4804695f6f94186b14f88ca
Berger Adrian <adrian.berger.3@students.bfh.ch>
2019-11-28 17:15:14 +0100

    Change annotation of Repository

--- camp/src/main/java/ch/bfh/autumn/repository/HeroRepository.java
+++ camp/src/main/java/ch/bfh/autumn/repository/HeroRepository.java
@@ -4,9 +4,10 @@ import ch.bfh.autumn.model.Hero;
 //import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource
 //public interface HeroRepository extends MongoRepository<Hero, String> {
 public interface HeroRepository extends CrudRepository<Hero, String> {
 
