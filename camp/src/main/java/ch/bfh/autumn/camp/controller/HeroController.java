31c2537134623f2852c6b30caa9caff465e56cfa
Berger Adrian <adrian.berger.3@students.bfh.ch>
2019-12-24 14:05:29 +0100

    Allow CrossOrigin for frontend application

--- camp/src/main/java/ch/bfh/autumn/camp/controller/HeroController.java
+++ camp/src/main/java/ch/bfh/autumn/camp/controller/HeroController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/heroesWOHateoas")
+@CrossOrigin(origins = "*")
 public class HeroController {
     @Autowired
     private HeroRepository heroRepository;
